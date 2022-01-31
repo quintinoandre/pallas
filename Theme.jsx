7457cdc5e530e4ae8ee96348a1b85df4bb82fd51
André Quintino <pandrequintino@gmail.com>
2022-01-31 23:01:03 +0000

    M01: L03: 04 – Form de Login

--- Theme.jsx
+++ Theme.jsx
@@ -5,4 +5,20 @@ export default {
 		fontSize: 20,
 		color: '#1F2937',
 	},
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#F2F4F6',
+	},
+	inputContainer: {
+		flex: 1,
+		marginTop: 30,
+		width: '90%',
+		padding: 20,
+		borderRadius: 10,
+		backgroundColor: '#fff',
+		alignItems: 'stretch',
+	},
+	backgroundColor: '#F2F4F6',
 };
--- src/public/Login/Login.jsx
+++ src/public/Login/Login.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image, useTheme } from 'react-native-elements';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Image, useTheme, Input } from 'react-native-elements';
+
+import { Feather as Icon } from '@expo/vector-icons';
 
 import logo from '../../../assets/logo.png';
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
 	logo: {
 		width: 100,
 		height: 100,
@@ -19,13 +15,37 @@ const styles = StyleSheet.create({
 });
 
 function Login() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
 	const { theme } = useTheme();
 
 	return (
-		<View style={styles.container}>
-			<Image source={logo} style={styles.logo} />
-			<Text style={theme.h1}>Sign in to our platform</Text>
-		</View>
+		<ScrollView style={{ backgroundColor: theme.backgroundColor }}>
+			<View style={theme.container}>
+				<Image source={logo} style={styles.logo} />
+				<Text style={theme.h1}>Sign in to our platform</Text>
+				<View style={theme.inputContainer}>
+					<Input
+						placeholder="Your email"
+						autoComplete="email"
+						autoCapitalize="none"
+						keyboardType="email-address"
+						value={email}
+						onChangeText={(text) => setEmail(text)}
+						leftIcon={<Icon size={24} color="black" name="user" />}
+					/>
+					<Input
+						placeholder="Your password"
+						autoComplete="password"
+						value={password}
+						secureTextEntry
+						onChangeText={(text) => setPassword(text)}
+						leftIcon={<Icon size={24} color="black" name="key" />}
+					/>
+				</View>
+			</View>
+		</ScrollView>
 	);
 }
 
