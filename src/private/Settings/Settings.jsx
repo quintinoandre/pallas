777c455582c419877fd6f9c7e0afefebe705c78a
André Quintino <pandrequintino@gmail.com>
2022-02-02 13:31:59 +0000

    M02: L01: 01 – Introdução a Settings

--- src/private/Settings/Settings.jsx
+++ src/private/Settings/Settings.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, ScrollView, View } from 'react-native';
+import { useTheme } from 'react-native-elements';
 
-function Settings() {
-	return <Text>Settings</Text>;
+function Settings({ ...props }) {
+	const { theme } = useTheme();
+
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [phone, setPhone] = useState('');
+	const [telegramChat, setTelegramChat] = useState('');
+	const [accessKey, setAccessKey] = useState('');
+	const [secretKey, setSecretKey] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
+	return (
+		<ScrollView>
+			<View style={theme.container}>
+				<View style={theme.inputContainer}>
+					<Text>Settings</Text>
+				</View>
+			</View>
+		</ScrollView>
+	);
 }
 
 export default Settings;
