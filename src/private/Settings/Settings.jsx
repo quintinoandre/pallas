3efdd6c7aec55c61436c6795d6c97e92e35b39eb
André Quintino <pandrequintino@gmail.com>
2022-02-02 14:01:26 +0000

    M02: L01: 02 – Formulário de Settings

--- src/private/Settings/Settings.jsx
+++ src/private/Settings/Settings.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Text, ScrollView, View } from 'react-native';
-import { useTheme } from 'react-native-elements';
+import { useTheme, Input, Button } from 'react-native-elements';
 
 function Settings({ ...props }) {
 	const { theme } = useTheme();
 
+	// eslint-disable-next-line no-unused-vars
+	const [limit, setLimit] = useState('');
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
@@ -19,7 +21,73 @@ function Settings({ ...props }) {
 		<ScrollView>
 			<View style={theme.container}>
 				<View style={theme.inputContainer}>
-					<Text>Settings</Text>
+					<Input
+						label="Name"
+						placeholder="John Doe"
+						autoComplete="name"
+						autoCapitalize="words"
+						onChangeText={(event) => setName(event)}
+						value={name}
+					/>
+					<Input
+						label="Email"
+						placeholder="contact@domain.com"
+						autoComplete="email"
+						autoCapitalize="none"
+						keyboardType="email-address"
+						onChangeText={(event) => setEmail(event)}
+						value={email}
+					/>
+					<Input label="Limit Plan" placeholder="none" value={limit} disabled />
+					<Input
+						label="New Password"
+						autoComplete="password-new"
+						autoCapitalize="none"
+						onChangeText={(event) => setPassword(event)}
+						value={password}
+						secureTextEntry
+					/>
+					{password ? (
+						<Input
+							label="Confirm Password"
+							autoCapitalize="none"
+							onChangeText={(event) => setConfirmPassword(event)}
+							value={confirmPassword}
+							secureTextEntry
+						/>
+					) : (
+						<></>
+					)}
+					<Input
+						label="Cellphone"
+						placeholder="+351123456789"
+						autoComplete="tel"
+						keyboardType="phone-pad"
+						onChangeText={(event) => setPhone(event)}
+						value={phone}
+					/>
+					<Input
+						label="Telegram Chat ID"
+						placeholder="-12345678"
+						keyboardType="numeric"
+						onChangeText={(event) => setTelegramChat(event)}
+						value={telegramChat}
+					/>
+					<Input
+						label="Access Key"
+						placeholder="Your access key"
+						autoComplete="none"
+						onChangeText={(event) => setAccessKey(event)}
+						value={accessKey}
+					/>
+					<Input
+						label="Secret Key"
+						placeholder="Your secret key"
+						autoComplete="none"
+						onChangeText={(event) => setSecretKey(event)}
+						value={secretKey}
+						secureTextEntry
+					/>
 				</View>
 			</View>
 		</ScrollView>
