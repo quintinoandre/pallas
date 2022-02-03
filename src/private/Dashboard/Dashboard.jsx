b52da30d96bf923d147c33a6cd5606f2576748b4
André Quintino <pandrequintino@gmail.com>
2022-02-03 18:01:07 +0000

    M02: L02: 07 – React-use-WebSocket

--- src/private/Dashboard/Dashboard.jsx
+++ src/private/Dashboard/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import useWebSocket from 'react-use-websocket';
+
+import { REACT_APP_BWS_URL } from '@env';
 
 import SelectSymbol from '../../components/SelectSymbol/SelectSymbol';
 import WalletSummary from '../../components/WalletSummary/WalletSummary';
@@ -7,10 +10,38 @@ import SymbolChart from './SymbolChart';
 
 function Dashboard() {
 	const [symbol, setSymbol] = useState('BTCUSDT');
+	const [data, setData] = useState({});
 
 	function onSymbolChange(event) {
 		setSymbol(event);
 	}
+
+	const { lastJsonMessage } = useWebSocket(
+		`${REACT_APP_BWS_URL}/${symbol.toLowerCase()}@ticker`,
+		{
+			onOpen: () => {},
+			onMessage: () => {
+				if (lastJsonMessage) {
+					setData({
+						priceChange: lastJsonMessage.p,
+						priceChangePercent: lastJsonMessage.P,
+						open: lastJsonMessage.o,
+						high: lastJsonMessage.h,
+						low: lastJsonMessage.l,
+						close: lastJsonMessage.c,
+						bestBid: lastJsonMessage.b,
+						bestAsk: lastJsonMessage.a,
+						numberOfTrades: lastJsonMessage.n,
+						quoteVolume: lastJsonMessage.q,
+						baseVolume: lastJsonMessage.v,
+					});
+				}
+			},
+			onError: (event) => console.error(event),
+			shouldReconnect: () => true,
+			reconnectInterval: 3000,
+		}
+	);
 
 	return (
 		<ScrollView>
@@ -20,6 +51,7 @@ function Dashboard() {
 			/>
 			<SymbolChart symbol={symbol} />
 			<WalletSummary symbol={symbol} />
+			<Text>{JSON.stringify(data)}</Text>
 		</ScrollView>
 	);
 }
