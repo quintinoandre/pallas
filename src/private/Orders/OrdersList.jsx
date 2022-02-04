05af84c82ff84ecc060c1ad708b88d020626f78b
André Quintino <pandrequintino@gmail.com>
2022-02-04 23:28:47 +0000

    M02: L04: 02 – Orders List

--- src/private/Orders/OrdersList.jsx
+++ src/private/Orders/OrdersList.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, View, StyleSheet } from 'react-native';
 
-import { NewOrderButton } from '../../components';
+import { NewOrderButton, SelectSymbol } from '../../components';
+
+const styles = StyleSheet.create({
+	emptyList: { flex: 1, alignItems: 'center', marginTop: 10 },
+});
 
 /**
  * props:
@@ -9,9 +13,35 @@ import { NewOrderButton } from '../../components';
  * - route?
  */
 function OrdersList({ ...props }) {
+	const [symbol, setSymbol] = useState('BTCUSDT');
+	const [orders, setOrders] = useState([
+		{ id: 1, value: 'test' },
+		{ id: 2, value: 'test2' },
+	]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const PAGE_SIZE = 10;
+
+	const emptyList = (
+		<View style={styles.emptyList}>
+			<Text>There are no orders for this symbol.</Text>
+		</View>
+	);
+
 	return (
 		<>
-			<Text>Orders List</Text>
+			<SelectSymbol
+				symbol={symbol}
+				onSymbolChange={(event) => setSymbol(event)}
+			/>
+			<FlatList
+				data={orders}
+				initialNumToRender={PAGE_SIZE}
+				refreshing={isLoading}
+				ListEmptyComponent={emptyList}
+				renderItem={(obj) => <Text>{obj.item.value}</Text>}
+				keyExtractor={(order) => order.id}
+			/>
 			<NewOrderButton navigation={props.navigation} symbol="" />
 		</>
 	);
